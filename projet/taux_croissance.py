0e08be33d7ac569635626646c7915597f191455e
keyz93 <80518856+keyz93@users.noreply.github.com>
2021-05-07 13:23:34 +0200

    Add files via upload

---
+++ projet/taux_croissance.py
@@ -0,0 +1,24 @@
+import pylab
+from modules import *
+
+
+pop_list = lireDonnee("data/evolution_pop.txt")
+
+
+
+list_annee = year_interval(1960, 2020)
+
+  
+x = array(list_annee)
+y = array(pop_list)
+ylabel("évolution de la population (en milliard)")
+
+xlabel("années")
+plot(x,y,'b:o')
+show()
+
+    
+    
+
+    
+    
